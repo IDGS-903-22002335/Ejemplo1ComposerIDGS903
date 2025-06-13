c83c26432b5b71d0600304d87f9bf3862901a0a6
IDGS-803-22002335 <tovardiazjuanaesmeralda@gmail.com>
2025-06-12 18:07:58 -0600

    Personajes Terminados 12/06/2025

--- app/src/main/java/com/esmeralda/ejemplo1composeridgs903/MainActivity.kt
+++ app/src/main/java/com/esmeralda/ejemplo1composeridgs903/MainActivity.kt
@@ -4,11 +4,14 @@ package com.esmeralda.ejemplo1composeridgs903
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import androidx. compose. runtime. getValue
+import androidx. compose. runtime. setValue
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -32,18 +35,22 @@ import androidx. compose. foundation. lazy. items
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Card
 
+
 import androidx. compose. foundation. shape. RoundedCornerShape
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 
 private val tarjeta: List<PersonajeTarjeta> = listOf(
-    PersonajeTarjeta("Gohan", "Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. "),
-    PersonajeTarjeta("Goku", "El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla."),
-    PersonajeTarjeta("Piccolo", "Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación."),
-    PersonajeTarjeta("Vegeta", "Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. "),
-    PersonajeTarjeta("Krillin","Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball."),
-    PersonajeTarjeta("Tenshinhan", "Maestro de las artes marciales y miembro de los Z Fighters."),
-    PersonajeTarjeta("Cell", "Cell conocido como Célula en España, es un bioandroide creado por la computadora del Dr. Gero, quien vino del futuro de la línea 3 con la intención de vengarse de Goku por haber acabado con el Ejército del Listón Rojo, y con ello el sueño de todo villano: dominar el mundo. "),
-    PersonajeTarjeta("Milk", "Esposa de Goku y madre de Gohan. "),
-    PersonajeTarjeta("Majin Buu", "También conocido como Boo original, es la forma original y pura de Majin-Boo, y la última forma de Boo que aparece en Dragon Ball Z")
+    PersonajeTarjeta("gohan", "Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. "),
+    PersonajeTarjeta("goku", "El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla."),
+    PersonajeTarjeta("piccolo", "Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación."),
+    PersonajeTarjeta("vegeta", "Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. "),
+    PersonajeTarjeta("krillin","Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball."),
+    PersonajeTarjeta("tenshinhan", "Maestro de las artes marciales y miembro de los Z Fighters."),
+    PersonajeTarjeta("cell", "Cell conocido como Célula en España, es un bioandroide creado por la computadora del Dr. Gero, quien vino del futuro de la línea 3 con la intención de vengarse de Goku por haber acabado con el Ejército del Listón Rojo, y con ello el sueño de todo villano: dominar el mundo. "),
+    PersonajeTarjeta("milk", "Esposa de Goku y madre de Gohan. "),
+    PersonajeTarjeta("majin Buu", "También conocido como Boo original, es la forma original y pura de Majin-Boo, y la última forma de Boo que aparece en Dragon Ball Z")
 
 )
 
@@ -66,10 +73,15 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ImagenHero(){
+fun ImagenHero(imageName: String){
+    val context = LocalContext.current
+    val ImageRestId = remember(imageName){
+        context.resources.getIdentifier(imageName.lowercase(),
+            "drawable", context.packageName)
+    }
     Image(
-             painter = painterResource(R.drawable.gohan),
-             contentDescription = "Gohan",
+             painter = painterResource(id=ImageRestId),
+             contentDescription = null,
              modifier = Modifier
              .padding(16.dp)
                  .size(100.dp)
@@ -113,7 +125,7 @@ fun MyPersonajes(personaje: PersonajeTarjeta){
                 .padding(8.dp)
                 .background(MaterialTheme.colorScheme.background)
         ) {
-            ImagenHero()
+            ImagenHero(personaje.title)
             Personajes(personaje)
         }
     }
@@ -121,13 +133,17 @@ fun MyPersonajes(personaje: PersonajeTarjeta){
 }
 
 @Composable
-fun Personaje(name:String, color:Color, style: TextStyle){
-    Text(text = name)
+fun Personaje(name:String, color:Color, style: TextStyle, lines: Int = Int.MAX_VALUE){
+    Text(text = name, color = color, style = style, maxLines=lines)
 
 }
 @Composable
 fun Personajes(personaje: PersonajeTarjeta){
-    Column{
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.padding(start = 8.dp).clickable {
+        expanded = !expanded
+    }){
         Personaje(personaje.title,
             MaterialTheme.colorScheme.primary,
             MaterialTheme.typography.headlineMedium)
@@ -135,6 +151,7 @@ fun Personajes(personaje: PersonajeTarjeta){
         Personaje(personaje.body,
             MaterialTheme.colorScheme.onBackground,
             MaterialTheme.typography.bodyLarge)
+           if(expanded) Int.MAX_VALUE else 1
     }
 
 
